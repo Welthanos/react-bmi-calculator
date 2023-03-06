a3879ac9c2dd4e7656b0309f0c8dd770197a9504
Welthanos <welthanos_dev@outlook.com>
2023-03-06 14:49:09 -0300

    refactor: calculation and style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import './app.css';
+import { useState } from 'react';
 
 function App() {
+  const [peso, setPeso] = useState('');
+  const [altura, setAltura] = useState('');
+  const [mensagem, setMensagem] = useState('');
+
+  function calcularIMC() {
+    const alt = altura / 100;
+    const imc = peso / alt ** 2;
+
+    if (imc < 18.5) {
+      setMensagem(`Seu IMC: ${imc.toFixed(2)} – Abaixo do peso!`)
+    } else if (imc >= 18.5 && imc < 25.0) {
+      setMensagem(`Seu IMC: ${imc.toFixed(2)} – Peso ideal!`)
+    } else if (imc >= 25.0 && imc < 30.0) {
+      setMensagem(`Seu IMC: ${imc.toFixed(2)} – Acima do peso!`)
+    } else if (imc >= 30.0 && imc < 35.0) {
+      setMensagem(`Seu IMC: ${imc.toFixed(2)} – Obesidade tipo I!`)
+    } else if (imc >= 35.0 && imc < 40.0) {
+      setMensagem(`Seu IMC: ${imc.toFixed(2)} – Obesidade tipo II!`)
+    } else if (imc >= 40.0) {
+      setMensagem(`Seu IMC: ${imc.toFixed(2)} – Obesidade mórbida!`)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Calculadora de IMC</h1>
+      <span>Vamos calcular o seu IMC</span>
+
+      <div className="area-input">
+        <input
+          type="text"
+          placeholder="Peso(kg) - Ex.: 90"
+          value={peso}
+          onChange={(e) => setPeso(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Altura(cm) - Ex.: 175"
+          value={altura}
+          onChange={(e) => setAltura(e.target.value)}
+        />
+
+        <button onClick={calcularIMC}>Calcular</button>
+
+      </div>
+
+      <h2>{mensagem}</h2>
     </div>
   );
 }
